f715fb344d990f0f39a5999b49a156ab2d7206b6
gerardo7996 <126217775+gerardo7996@users.noreply.github.com>
2023-06-22 16:45:05 -0600

    Archivo de JavaScript Modificado

---
+++ js/validar-subida.js
@@ -0,0 +1,92 @@
+//Mediante este código de JavaScript realizamos la validación
+//del formulario de subida de imágenes
+$(document).ready(function(){
+    $('#enviar-imagen').click(function(){
+        if ($('#codigo').val() == "") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1000,
+                timerProgressBar: false,
+            })
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Código Vacío!',
+                text: '¡Ingresa el código del producto!',
+                showConfirmButton: true,
+                confirmButtonColor: '#0091EA',
+                confirmButtonText: 'Aceptar'
+            })
+            return false;
+        } else if ($('#descripcion').val() == "") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1000,
+                timerProgressBar: false,
+            })
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Descripción Vacía!',
+                text: '¡Ingresa la descripción del producto!',
+                showConfirmButton: true,
+                confirmButtonColor: '#0091EA',
+                confirmButtonText: 'Aceptar'
+            })
+            return false;
+        } else if ($('#precio').val() == "") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1000,
+                timerProgressBar: false,
+            })
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Precio Vacío!',
+                text: '¡Ingresa el precio del producto!',
+                showConfirmButton: true,
+                confirmButtonColor: '#0091EA',
+                confirmButtonText: 'Aceptar'
+            })
+            return false;
+        } else if ($('#stock').val() == "") {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1000,
+                timerProgressBar: false,
+            })
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Stock Vacío!',
+                text: '¡Ingresa el stock del producto!',
+                showConfirmButton: true,
+                confirmButtonColor: '#0091EA',
+                confirmButtonText: 'Aceptar'
+            })
+            return false;
+        } else if (condition) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1000,
+                timerProgressBar: false,
+            })
+            Swal.fire({
+                icon: 'warning',
+                title: '¡Imagen no seleccionada!',
+                text: '¡Selecciona una imagen de un producto!',
+                showConfirmButton: true,
+                confirmButtonColor: '#0091EA',
+                confirmButtonText: 'Aceptar'
+            })
+            return false;
+        }
+    });
+});
