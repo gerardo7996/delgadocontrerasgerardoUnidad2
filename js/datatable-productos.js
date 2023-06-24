89009baaddc28da811f19d9d624f26edbf690887
gerardo7996 <126217775+gerardo7996@users.noreply.github.com>
2023-06-24 17:40:46 -0600

    Delete datatable-productos.js

--- js/datatable-productos.js
+++
@@ -1,7 +0,0 @@
-//Mediante uso de este script de JavaScript
-//vamos a generar la DataTable de la tabla de productos
-let tabla_productos = new DataTable('#tabla_productos');
-
-$(document).ready(function(){
-	$('#tabla_productos').DataTable();
-});
